0e957767b63ced82f259384c8a95e0cb4912367b
Tim Zabawa <timothyzabawa@gmail.com>
2022-04-09 06:27:50 -0400

    Add initial event-bus service boilderplate code

---
+++ blog/event-bus/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+    const { body: event } = req;
+
+
+    try {
+        await Promise.all([
+            axios.post('http:/localhost:4000/events', event),
+            axios.post('http:/localhost:4001/events', event),
+            axios.post('http:/localhost:4002/events', event)
+        ])
+
+        res.send({status: 'OK'});
+    } catch (error) {
+        res.send({status: 'ERROR', error});
+    }   
+});
+
+app.listen(4005, () => {
+    console.log('Listening on port 4005');
+})
